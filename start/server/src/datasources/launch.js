f7ed43600ef901717e7399510b457c9c04b2cbb5
tiffanybacher <tiffanybacher@gmail.com>
2019-08-27 16:17:38 -0600

    Create lauchReducer in LaunchAPI

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -8,11 +8,30 @@ class LaunchAPI extends RESTDataSource {
 
   async getAllLaunches() {
     const response = await this.get('launches');
-    
+
     return Array.isArray(response)
       ? response.map(launch => this.launchReducer(launch))
       : [];
   }
+
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
 }
 
 module.exports = LaunchAPI;
