e1c58cfe3ef548475b627dbf6ac6da89a3a5adb5
tiffanybacher <tiffanybacher@gmail.com>
2019-08-27 15:46:38 -0600

    Create an object type to define structure of Launch

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -7,6 +7,14 @@ const typeDefs = gql`
     # Queries for the current user
     me: User
   }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 `;
 
 module.exports = typeDefs;
