94eae68bb9f211921405828c911c6d72a1d74223
tiffanybacher <tiffanybacher@gmail.com>
2019-08-27 21:46:04 -0600

    Update Mission object type to take in a parent argument

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -49,7 +49,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
